a84f7e7d1e3ba06c00a5174cfa55bd7ae65d757b
sruiy <sry0009@foxmail.com>
2019-08-31 19:37:43 +0800

    初始化项目-页面分析-新建登录和主页

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Home from './views/home'
+import Login from './views/login'
 
 Vue.use(Router)
 
@@ -8,8 +9,15 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
       component: Home
+    },
+    {
+      path: '/login',
+      component: Login
     }
     // {
     //   path: '/about',
