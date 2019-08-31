5ab0468ee777f71084cbd1331543c00ae2d39221
sruiy <sry0009@foxmail.com>
2019-08-31 19:18:36 +0800

    初始化项目-整理目录-准备资源

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import elementUi from 'element-ui'
+import './style/index.less'
+Vue.use(elementUi)
 
 Vue.config.productionTip = false
 
