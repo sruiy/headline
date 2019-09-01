c7d0117fc4fdfb2e5d276294ea9f01b1a7269a7f
sruiy <sry0009@foxmail.com>
2019-09-01 20:25:29 +0800

    主页模块-头部结构及下拉菜单

---
+++ src/components/home/header.vue
@@ -0,0 +1,51 @@
+<template>
+  <el-row type="flex" justify="space-between">
+    <el-col :span="8">
+      <div class="grid-content bg-purple header-left">
+        <span class="el-icon-s-fold"></span>
+        江苏传智播客教育科技股份有限公司
+      </div>
+    </el-col>
+    <el-col :span="3" class="header-right">
+      <div class="grid-content bg-purple">
+        <img src="../../assets/img/avatar.jpg" alt />
+        <el-dropdown trigger="click">
+          <span class="el-dropdown-link">
+            用户名
+            <i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>个人信息</el-dropdown-item>
+            <el-dropdown-item>git地址</el-dropdown-item>
+            <el-dropdown-item divided>退出</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </div>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+.header-left {
+  margin-top: 15px;
+  span {
+    font-size: 20px;
+    vertical-align: bottom;
+    margin-right: 3px;
+  }
+}
+.header-right {
+    margin-top: 5px;
+  img {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    vertical-align: middle;
+    margin-right: 5px;
+  }
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,26 +1,30 @@
 <template>
   <el-container>
-    <el-aside>
+    <el-aside style="width:200px">
       <home-aside></home-aside>
     </el-aside>
-  <el-container>
-    <el-header>顶部</el-header>
-    <el-main>主页</el-main>
-  </el-container>
+    <!--头部-->
+    <el-container>
+      <el-header style="height:50px">
+        <home-header></home-header>
+      </el-header>
 
+      <el-main>主页</el-main>
+    </el-container>
   </el-container>
 </template>
 
 <script>
 import homeAside from '../../components/home/aside'
+import homeHeadr from '../../components/home/header'
 export default {
 
   components: {
-    'home-aside': homeAside
+    'home-aside': homeAside,
+    'home-header': homeHeadr
   }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
 </style>
