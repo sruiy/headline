dbdd4271c0e33800893727b11ce4bfdae0921551
sruiy <sry0009@foxmail.com>
2019-09-02 10:34:48 +0800

    主页模块-导航守卫

---
+++ src/beforeRouter.js
@@ -0,0 +1,15 @@
+import router from './router'
+router.beforeEach((to, from, next) => {
+  if (to.path.startsWith('/home')) {
+    let token = window.localStorage.getItem('token')
+    if (token) {
+      next()
+    } else {
+      router.push('/login')
+    }
+  } else {
+    next()
+  }
+})
+
+export default router
