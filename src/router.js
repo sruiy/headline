4295f179b58eee9ab8623e4ed3f3c0284dc74073
sruiy <sry0009@foxmail.com>
2019-09-03 09:32:17 +0800

    一些改动

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from './views/login'
 import Home from './views/home'
-import homeMain from './components/home/secondRouter/homemain.vue'
+import homeMain from './views/homemain.vue'
 
 Vue.use(Router)
 
